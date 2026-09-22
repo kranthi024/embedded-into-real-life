72c2fe316b5f70c91564237744179ff1bb92a844
kranthi024 <kranthikumarg4@gmail.com>
2026-09-22 15:30:40 +0530

    Add files via upload

--- ContactModal.tsx
+++ ContactModal.tsx
@@ -93,7 +93,7 @@ export default function ContactModal({
           <div className="rounded-2xl border border-white/10 bg-white/[0.03] p-5">
 
             <p className="text-xs uppercase tracking-[0.2em] text-gray-500">
-              Founder&CEO
+              Founder
             </p>
 
             <h3 className="mt-2 text-xl font-semibold text-white">
